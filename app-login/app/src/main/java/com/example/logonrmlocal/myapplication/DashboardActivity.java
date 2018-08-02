b545277640ab04a424aca876dd1cb9b7875784ce
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-02 20:29:43 -0300

    Adicionando o app login

---
+++ app-login/app/src/main/java/com/example/logonrmlocal/myapplication/DashboardActivity.java
@@ -0,0 +1,28 @@
+package com.example.logonrmlocal.myapplication;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        sp = getSharedPreferences("login",MODE_PRIVATE);
+    }
+
+    public void logout(View view) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.remove("usuario");
+        editor.remove("senha");
+        editor.remove("conectado");
+        editor.commit();
+        finish();
+    }
+}
